c81d7e345ba5a9a1adab649a1d8b6665e837d98c
patrickkosasih <77109385+patrickkosasih@users.noreply.github.com>
2023-07-13 13:26:55 +0700

    Player actions and basic game flow
    Each player can now take action on a betting round:
    * Fold
    * Check/call
    * Bet/raise
    
    The program now has a basic game flow, such as going to the next betting round after everyone has bet the same amount. However, win detectors and the end of a hand haven't been implemented yet.
    
    Before an input, the state of the game is printed to the console, which are the player states, pot, and community cards. The user then inputs the action of each player onto the console.

--- rules.py
+++ rules.py
@@ -21,10 +21,10 @@ forward, while letter ranks are represented by the numbers 1, 11, 12, and 13:
 * 13 : K (King)
 
 The suit of a card is represented by a single character, which is the initial letter of the suit name.
-* "S" : Spades
-* "H" : Hearts
-* "D" : Diamonds
-* "C" : Clubs
+* "S" : ♠ Spades
+* "H" : ♥ Hearts
+* "D" : ♦ Diamonds
+* "C" : ♣ Clubs
 """
 
 SUIT_CHARS = {"S": "♠",
@@ -38,6 +38,14 @@ RANK_CHARS = {1: "A",
               13: "K"}
 
 
+class Actions:
+    FOLD = 0
+    CHECK = 1
+    CALL = 1
+    BET = 2
+    RAISE = 2
+
+
 def card_str(card: Card):
     """
     Converts a `Card` namedtuple into a readable string format.
@@ -49,10 +57,14 @@ def card_str(card: Card):
 class PlayerData:
     """
     The `PlayerData` class contains attributes of a player playing throughout the different hands of a poker game.
+
+    The difference between this class to `PlayerHand` is that the attributes of this class is brought on through all
+    the different hands of a game of poker, such as the amount of money.
     """
 
-    def __init__(self):
-        self.money = 1000
+    def __init__(self, name: str, money: int):
+        self.name = name
+        self.money = money
 
 
 class PlayerHand:
@@ -63,15 +75,38 @@ class PlayerHand:
     the current hand, such as the pocket cards of a player.
     """
 
-    def __init__(self, player_data: PlayerData):
+    def __init__(self, hand: "Hand", player_data: PlayerData):
+        self.hand = hand
         self.player_data = player_data
         self.pocket_cards = []
-        self.bet_amount = 0
-        self.folded = False
 
-    def bet(self, new_amount: int):
-        self.player_data.money -= new_amount - self.bet_amount
-        self.bet_amount = new_amount
+        self.bet_amount = 0  # The amount of money spent on the current betting round
+        self.folded = False
+        self.called = False
+
+    def bet(self, new_bet: int, blinds=False):
+        """
+        Pay an amount of money to match the bet of other players.
+
+        :param new_bet: The equal bet amount.
+        :param blinds: If set to True, then `called` will not be set to True. Used on the start of a hand where the
+        two players with the blinds must bet an amount of money but still must call/check later.
+
+        :return:
+        """
+        amount_to_pay = new_bet - self.bet_amount  # How much money to pay to call/raise
+
+        if amount_to_pay >= self.player_data.money:
+            # ALL IN
+            amount_to_pay = self.player_data.money
+            new_bet = self.player_data.money
+
+        self.player_data.money -= amount_to_pay
+        self.hand.pot += amount_to_pay
+        self.bet_amount = new_bet
+
+        if not blinds:
+            self.called = True
 
 
 class Hand:
@@ -82,24 +117,134 @@ class Hand:
 
     def __init__(self, game: "PokerGame"):
         self.game = game
-        self.player_hands = [PlayerHand(player) for player in game.players]
+        self.players = [PlayerHand(hand=self, player_data=player) for player in game.players]
+        # A list of `PlayerHand` instances based on the `PlayerData` list of the `PokerGame`.
 
         self.pot = 0
-        self.community_cards = []
+        self.bet_amount = self.game.sb_amount * 2
 
+        self.community_cards = []
         self.deck = [Card(rank, suit) for suit in "SHDC" for rank in range(1, 14)]
         random.shuffle(self.deck)
 
         """
         Deal cards to players
         """
-        n_dealed_cards = len(self.player_hands) * 2
+        n_dealed_cards = len(self.players) * 2
 
         dealed_cards = self.deck[:n_dealed_cards]
         self.deck = self.deck[n_dealed_cards:]
 
-        for i, player in enumerate(self.player_hands):
+        for i, player in enumerate(self.players):
             player.pocket_cards = dealed_cards[i * 2 : i * 2 + 2]
+
+        """
+        Player turn initialization and blinds
+        """
+        self.current_turn = self.game.dealer
+        self.blinds = sb, bb = self.get_next_turn(1), self.get_next_turn(2)
+
+        self.players[sb].bet(self.game.sb_amount, blinds=True)
+        self.players[bb].bet(self.game.sb_amount * 2, blinds=True)
+
+        self.current_turn = self.get_next_turn(3)
+        # The player with the first turn of a new hand is the player after the big blinds
+
+    def action(self, action_type: int, new_amount=0):
+        """
+        Takes an action for the current turn player. There are 3 types of actions:
+        1. Fold
+        2. Check/call
+        3. Bet/raise
+
+        :param action_type: Determines the type of action using an integer value from the constants of the `Actions`
+        class (0-2).
+
+        :param new_amount: The amount of money to bet. Only used when the action type is to bet/raise.
+
+        :return:
+        """
+        if type(action_type) is not int:
+            raise TypeError("action type must be an int")
+        elif action_type not in range(3):
+            raise ValueError(f"action type must be 0, 1, or 2 (got: {action_type})")
+
+        match action_type:
+            case Actions.FOLD:   # Fold
+                self.get_current_player().folded = True
+
+            case Actions.CALL:   # Check/call
+                self.get_current_player().bet(self.bet_amount)
+                self.get_current_player().called = True
+
+            case Actions.RAISE:  # Bet/raise
+                if new_amount <= 2 * self.game.sb_amount:
+                    raise ValueError("bet amount must be more than the minimum bet amount"
+                                     f"({2 * self.game.sb_amount})")
+                elif new_amount <= self.bet_amount:
+                    raise ValueError("the new bet amount must be more than the previous betting amount "
+                                     f"({self.bet_amount})")
+
+                # Everyone except the betting/raising player must call again
+                for x in self.players:
+                    x.called = False
+
+                self.bet_amount = new_amount
+                self.get_current_player().bet(new_amount)
+
+        if all(player.called for player in self.players if not player.folded):
+            self.next_round()
+        else:
+            self.current_turn = self.get_next_turn()
+
+    def next_round(self):
+        """
+        When everyone who is still in has bet the same amount, the game advances to the next betting round.
+
+        :return:
+        """
+        for player in self.players:
+            player.bet_amount = 0
+            player.called = False
+
+        self.bet_amount = 0
+        self.current_turn = self.get_next_turn(1, turn=self.game.dealer)
+
+        draw_n_cards = 3 if len(self.community_cards) == 0 else 1
+        for _ in range(draw_n_cards):
+            self.community_cards.append(self.deck.pop(0))
+
+        print("NEXT BETTING ROUND")
+
+    def get_next_turn(self, n=1, turn=-1, first_call=True) -> int:
+        """
+        Returns the player index after `n` turns of the current player turn.
+        Players who have folded or are all-in are skipped.
+
+        e.g. n = 2: returns the player index after 2 turns
+
+        The `turn` argument may be passed in. If the `turn` argument is not passed in then this function uses the
+        `self.current_turn` attribute.
+
+        :param n: Number of turns after the specified turn
+        :param turn: The current turn. If the argument is not passed in then the `self.current_turn` attribute is used.
+        :param first_call: Always True when calling this function from outside, and then False for the recursions that
+        follow. This is done so that the player of the `turn` parameter on the first call is not immediately skipped to
+        the next player if the player on the first call has already folded/all-in.
+
+        :return: The player index after `n` turns.
+        """
+        turn = self.current_turn if turn < 0 else turn
+
+        if not first_call and (self.players[turn].folded or self.players[turn].player_data.money <= 0):
+            return self.get_next_turn(n=n, turn=(turn + 1) % len(self.players), first_call=False)
+        elif n <= 0:
+            return turn
+        else:
+            return self.get_next_turn(n=n-1, turn=(turn + 1) % len(self.players), first_call=False)
+
+    def get_current_player(self) -> PlayerHand:
+        return self.players[self.current_turn]
 
 
 class PokerGame:
@@ -111,11 +256,15 @@ class PokerGame:
     def __init__(self, n_players: int):
         # Note: In the future players may join in the middle of an ongoing match and the `n_players` parameter won't
         # be necessary.
-        self.players = [PlayerData() for _ in range(n_players)]
-        self.dealer = 0
-        self.hand: Hand = Hand(self)
+        self.players = [PlayerData(f"Player {i + 1}", 1000) for i in range(n_players)]
+
+        self.dealer = 0  # The index of `self.players` who becomes the dealer of the current hand.
+        self.sb_amount = 25  # Small blinds amount. Big blinds = 2 * Small blinds.
+
+        self.current_hand = Hand(self)
 
     def new_hand(self):
         # Cycle dealer
         self.dealer = (self.dealer + 1) % len(self.players)
-        self.hand = Hand(self)
+
+        self.current_hand = Hand(self)
--- testing.py
+++ testing.py
@@ -7,10 +7,95 @@ A module for early testing and basic interface to the poker game before the GUI 
 import rules
 
 
+def print_state(game: rules.PokerGame):
+    """
+    Prints the state of the current game/hand:
+
+    1. Player states:
+        a. An arrow (->) for the current turn player
+        b. Player name
+        c. Pocket cards
+        d. Money
+        e. Called/folded?
+        f. Dealer, small blinds, big blinds (only on preflop round)
+
+    2. Pot
+    3. Community cards
+
+    """
+
+    for player in game.current_hand.players:
+        print(f"{'-> ' if player is game.current_hand.get_current_player() else '   '}"
+              f"{player.player_data.name}: {[rules.card_str(card) for card in player.pocket_cards]}; "
+              f"${player.player_data.money:,}; "
+              f"{'Folded' if player.folded else ('Called' if player.called else '')} ", end="")
+
+        if not game.current_hand.community_cards:
+            # If the current round is still the preflop round then determine the D, SB, and BB
+            players = game.current_hand.players
+
+            if player is players[game.dealer]:
+                print("D")
+            elif player is players[game.current_hand.blinds[0]]:
+                print("SB")
+            elif player is players[game.current_hand.blinds[1]]:
+                print("BB")
+            else:
+                print()
+
+        else:
+            print()
+
+
+    print(f"\nPot: {game.current_hand.pot}\n"
+          f"Community cards: {[rules.card_str(card) for card in game.current_hand.community_cards]}")
+
+
 def standard_io_poker():
-    game = rules.PokerGame(4)
+    """
+    Run a text based poker game using the console's basic input and output.
 
-    print([rules.card_str(card) for card in game.hand.deck])
+    To play, type in the actions of each player (not case-sensitive). Actions can be the following:
+    1. Fold
+    2. Check
+    3. Call
+    4. Bet <bet amount>
+    5. Raise <new bet amount>
 
-    for i, player in enumerate(game.hand.player_hands):
-        print(f"Player {i + 1}: {[rules.card_str(card) for card in player.pocket_cards]} ; ${player.player_data.money:,}")
+    Note that check and call does the same thing; and bet and raise also does the same thing.
+    """
+
+    game = rules.PokerGame(6)
+
+    # print([rules.card_str(card) for card in game.current_hand.deck])
+
+    while True:
+        print("\n" + "=" * 50 + "\n")
+        print_state(game)
+        print()
+
+        player_name = game.current_hand.get_current_player().player_data.name  # Name of the current turn player
+        action = input(f"What will {player_name} do? ").upper().split()
+        # The input is uppercased and then split into a list.
+        new_amount = 0
+        # New amount for betting/raising
+
+        try:
+            if action[0] == "QUIT":
+                break
+
+            if action[0] in ("BET", "RAISE"):
+                if len(action) < 2:
+                    print("Specify a betting amount.")
+                    continue
+
+                new_amount = int(action[1])
+
+            action_code = rules.Actions.__dict__[action[0]]
+            game.current_hand.action(action_code, new_amount)
+
+        except (IndexError, KeyError):
+            print("Invalid input.")
+
+        except ValueError as e:
+            print("Invalid betting amount:", e)
